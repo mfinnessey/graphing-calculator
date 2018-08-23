c05fda9cf77ba8e5fee7228f1ea30a353835ea94
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-08-23 10:58:51 -0700

    Window
    Added a window for the calculator. Nothing major.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,17 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Label;
+import javax.swing.JFrame;
 
 public class Calculator {
 // TODO a class to model a graphing calculator.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		JFrame frame = new JFrame();
+		Label yellowLabel = new Label("Graphing Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+		}
 }
