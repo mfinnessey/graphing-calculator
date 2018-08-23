f02fef61a4289d533f6bdb7160f62ad931383ff1
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-08-23 12:05:13 -0700

    Working JButton and JTextField
    Got a JButton and JTextfield working (by some miracle, IDK how it's working in a large part). I'm probably going to pull this into master when I can integrate it with the MathEvaluator in some way.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,44 @@
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Label;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class Calculator {
-// TODO a class to model a graphing calculator.
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.event.KeyListener;
+
+public class Calculator implements ActionListener{
+	// TODO a class to model a graphing calculator
+	public Calculator() {
+		KeyListener key = null;
 		JFrame frame = new JFrame();
-		Label yellowLabel = new Label("Graphing Calculator");
+		JTextField equation = new JTextField("Enter an equation.");
+		equation.addKeyListener(key);
+		JButton calculate = new JButton("Calculate");
+		calculate.setMnemonic(KeyEvent.VK_ENTER);
+		calculate.addActionListener(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+		frame.getContentPane().add(calculate, BorderLayout.CENTER);
+		frame.getContentPane().add(equation, BorderLayout.SOUTH);
 		frame.pack();
 		frame.setVisible(true);
-		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		
+	   	MathEvaluator m = new MathEvaluator("x^2");		
+	   	m.addVariable("x", ((2.0)));
+	   	System.out.println(m.getValue());
+	   	//double value_test = m.getValue();
+	   	//System.out.println("is equation null?   " + value_test);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Click!");
+	}
 }
