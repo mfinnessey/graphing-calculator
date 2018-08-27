034b133ae0e520b8c5049eecd7c956feb0210f11
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-08-27 11:39:03 -0700

    Storing Values in Array
    Storing values calculated by the MathEvaluator in an array. From here we can probably start implementing the graphing thing next. Then the calculus.

--- src/GUI.java
+++ src/GUI.java
@@ -7,8 +7,9 @@ import javax.swing.JTextField;
 
 public class GUI {
 	// TODO a class to model a graphing calculator
+	int indexTracker = 0;
+	double [] yValues = new double [11];
 	public GUI() {
-		
 		JFrame frame = new JFrame();
 		final JTextField equation = new JTextField("Enter an equation.");
 		JButton calculate = new JButton("Calculate");
@@ -21,10 +22,16 @@ public class GUI {
 	   	m.addVariable("x", ((2.0)));
 	   	calculate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				System.out.println("Equation: " + equation.getText());
 				m.setExpression(equation.getText());
-				System.out.println("Equation value = " + m.getValue());
+				for(int i = -5; i <= 5; i++) {
+					m.addVariable("x", (i));
+					System.out.println(m.getValue());
+					yValues[indexTracker++] = m.getValue();
+				}
+				for(int i = 0; i <= 10; i++) {
+					System.out.println((i-5) + "," + yValues[i]);
+				}
 			}
 	   	});
 	}
