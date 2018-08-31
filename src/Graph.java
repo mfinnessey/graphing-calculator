bbf77bce4c60339e431aa66c0d8ac413bbea76b0
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-08-31 13:52:09 -0700

    Trying Random Crap
    Stay tuned for more. Possible GeneralPath

--- src/Graph.java
+++ src/Graph.java
@@ -1,23 +1,19 @@
 import java.awt.*;
+import java.awt.geom.Path2D;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Graph extends JPanel {
-	Graphics g;
-	public void drawGraph(Graphics g, int [] xValues, int [] yValues) {
-		// This should explain how to fix the NullPointerException: https://stackoverflow.com/questions/12335121/initializing-a-graphics-variable
-		g.setColor(Color.decode("0x45e5B"));
-		g.drawPolyline(yValues, yValues, xValues.length);
-	}
+	Path2D.Double path = new Path2D.Double();
 	
-	public Graphics getClassGraphics() {
-		return g;
-	}
+	
+	
 	
 	
 	public static void main(String[] args) {
 		GUI gui = new GUI();
 		Graph graph = new Graph();
-		JFrame f = new JFrame("Twilight Zone");
+		JFrame f = new JFrame("Graph");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().add(graph);
 		f.setSize(290, 325);
@@ -25,8 +21,6 @@ public class Graph extends JPanel {
 		f.setVisible(true);
 		/* There might be a synchronization issue here so I'm trying this. 
 		 * We won't know for sure until we fix the NullPointerException in drawGraphics()*/
-		while(true) {
-			graph.drawGraph(graph.getClassGraphics() , gui.getXValues(), gui.getYValues());
-		}
+	
 	}
 }
