d50e8339a184e3314c45d36e9bd2df3e6d33fa49
iuhnivek <iuhnivek@gmail.com>
2018-09-05 15:01:23 -0700

    idk

--- src/Derivative.java
+++ src/Derivative.java
@@ -1,4 +1,13 @@
+import MathEvaluator;
 
 public class Derivative {
-
+	
+	public static void main(String[] args) {
+		MathEvaluator.main(args);
+		
+	}
+	
+	public static String firstDerivative(String s) {
+		MathEvaluator evaluator = new MathEvaluator(s);
+	}
 }
