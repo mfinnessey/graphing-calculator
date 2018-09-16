60dab66b6b8833179c82555f0997aaae63f76d94
iuhnivek <iuhnivek@gmail.com>
2018-09-16 03:01:34 -0700

    zeros

--- src/Zeroes.java
+++ src/Zeroes.java
@@ -5,4 +5,27 @@ public class Zeroes {
 	 * K: Problem: if a function touches the x axis but doesn't cross
 	 * K: No solution yet
 	 */
+	MathEvaluator ME = new MathEvaluator();
+	
+	public static int splitter(String s) {
+		// M Splitting the polynomial expression at every term
+		// M Let's hope we won't have to simplify anything. We should check in on this.
+		// M Also we need to find a way to store the + or - as split doesn't retain it.
+		String[] terms = s.split("\\+|\\-");
+		int splitIndex = 0;
+		for(int i = 0; i < terms.length; i++) {
+			terms[i].trim();
+			for(int j = 0; j < terms[i].length(); j++) {
+				if (terms[i].charAt(j) == 'x'){
+					splitIndex = j;
+				}
+			}
+		}
+		return splitIndex;
+	}
+	
+	public static double plugIn(String s, double xval) {
+		double rV = 0;
+		return rV;
+	}
 }
