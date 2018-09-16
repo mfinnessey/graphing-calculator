a38eae43b08a6be93f2a4da0d4ba4b6eea40d908
iuhnivek <iuhnivek@gmail.com>
2018-09-16 02:26:59 -0700

    zeros function

---
+++ src/Zeroes.java
@@ -0,0 +1,8 @@
+
+public class Zeroes {
+//TODO: create a class to find zeros and other values using IVT
+	/* K: Current idea is to use a for looop and increase i by 0.001 and use IVT to chekc if there's a 0
+	 * K: Problem: if a function touches the x axis but doesn't cross
+	 * K: No solution yet
+	 */
+}
