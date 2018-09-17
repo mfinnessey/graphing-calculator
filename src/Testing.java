b42071fbad94734815c204f6f85e29e383e5e830
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-09-17 13:53:35 -0700

    More Broken
    It's just more broken. It's hard to fill the array.

--- src/Testing.java
+++ src/Testing.java
@@ -6,10 +6,25 @@ public class Testing {
 		// TODO Auto-generated method stub
 		final double step = Math.pow(10, -7);
 		//M Currently issues with precision with this.
-		DoubleStream stream = DoubleStream.iterate(-10, d->d+=step);
-		double[] array = stream.limit((long) (20/step)).toArray();
-		for(int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		DoubleStream stream = DoubleStream.iterate(-10, d->d + step);
+		double[] rawArray = stream.limit((long) (20/step)).toArray();
+		//M index at which the filteredArray should be filled.
+		int fillIndex = 0;
+		double [] filteredArray = new double[rawArray.length];
+		for(int i = 0; i < rawArray.length; i++) {
+			System.out.println(rawArray[i]);
 		}
+		//M The runtime for this is currently stupid with step = 10^-8
+		/* for(int i = 0; i < rawArray.length; i++) {
+			for(int j = 0; j < filteredArray.length; j++) {
+				if(rawArray[i] == filteredArray[j]) {
+					break;
+				}
+			}
+			filteredArray[fillIndex++] = rawArray[i];
+		}
+		for(int i = 0; i < filteredArray.length; i++) {
+			System.out.println(filteredArray[i]);
+		} */
 	}
 }
