156589509d32ec2ea9fba64c12b20ba2d1ddcfb7
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-09-18 14:02:07 -0700

    It's Alive!
    The project works again, perfect! I'll probably move this back to the GUI class once I can fix the performance problem with rounding (which is going to take minutes if not hours currently just to accurately fill the xValues, God forbid the yValues). Let's talk about reducing precision requirements for most of the graph and then finding a way to identify key intervals on which it needs to be increased.

--- src/Testing.java
+++ src/Testing.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.stream.DoubleStream;
 
 public class Testing {
@@ -5,15 +7,29 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		final double step = Math.pow(10, -7);
+		int indexTracker = 0;
+		// final BigDecimal bd = new BigDecimal(Math.pow(10, -8));
+		double [] xValues = new double[200000001];
+		for(int i = (int) (-1 * Math.pow(10,8)); i <= (int) Math.pow(10, 8); i++) {
+			xValues[indexTracker++] = (double) (i * step);
+		}
+		for(int j = 0; j < indexTracker; j++) {
+			xValues[j] = round(xValues[j]);
+			System.out.println(xValues[j]);
+		}
+		
+		
+		
+		// final double step = Math.pow(10, -8);
 		//M Currently issues with precision with this.
-		DoubleStream stream = DoubleStream.iterate(-10, d->d + step);
+		/* DoubleStream stream = DoubleStream.iterate(-10, d -> d + step);
 		double[] rawArray = stream.limit((long) (20/step)).toArray();
 		//M index at which the filteredArray should be filled.
 		int fillIndex = 0;
 		double [] filteredArray = new double[rawArray.length];
 		for(int i = 0; i < rawArray.length; i++) {
 			System.out.println(rawArray[i]);
-		}
+		} /*
 		//M The runtime for this is currently stupid with step = 10^-8
 		/* for(int i = 0; i < rawArray.length; i++) {
 			for(int j = 0; j < filteredArray.length; j++) {
@@ -26,5 +42,10 @@ public class Testing {
 		for(int i = 0; i < filteredArray.length; i++) {
 			System.out.println(filteredArray[i]);
 		} */
+	}
+	public static double round(double d) {
+		BigDecimal bd = new BigDecimal(d);
+		bd = bd.setScale(7, RoundingMode.HALF_UP);
+		return bd.doubleValue();
 	}
 }
