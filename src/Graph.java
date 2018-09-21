3895f5ad9264bde9b9246b0d3ddbd4194670e434
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-09-21 13:42:33 -0700

    QoL Changes
    Changed the precision to 10^-4, which seems to have acceptable performance, but also reduces the dashing that existed with functions like tan(x) at 10^-2. Currently there are some issues with graphing complex polynomial functions, but this could just be an issue with MathEvaluator. Now I'm going to move on to derivatives for now.

--- src/Graph.java
+++ src/Graph.java
@@ -4,17 +4,19 @@ import java.awt.geom.Line2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+//M Currently issues with graphing complex polynomial functions, likely an issue with syntax for MathEvaluator.
 //M Added these things to hopefully get some drawings gong.
 public class Graph {
 	//M Adding a new JPanel on which to hold the things.
 	JPanel graphWindow = new JPanel();
 	public Graph() {
 		JFrame frame = new JFrame();
-		graphWindow.setSize(500, 500);
-		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(graphWindow, BorderLayout.CENTER);
 		frame.pack();
+		frame.setSize(500, 500);
+		graphWindow.setSize(500, 500);
+		frame.setLocation(500, 0);
 		frame.setVisible(true);
 	}
 	 public void draw(double [] xValues, double [] yValues) {
@@ -30,7 +32,6 @@ public class Graph {
 	        //drawString draws a string onto the screen
 
 	        //Plot the data points, using our method
-	        System.out.println("Beginning to draw a point");
 	        g.draw(new Line2D.Double(xValues[0], yValues[0], xValues[0], yValues[0]));
 	        for (int i=1; i < xValues.length; i++)
 	            drawPoint(xValues[i], yValues[i]);  //Your data gets read here
@@ -38,9 +39,8 @@ public class Graph {
 
 	        private void drawPoint(double x, double y) {
 	        	//TODO A method to draw the various lines that make up the graph.
-	        	System.out.println("Drawing the point (" + x + ", " + y + ").");
-	        	//M Implementing behavior to scale with the axes, more can be added with more time.
-	            g.draw(new Line2D.Double((x + 250), (-y + 250), (x + 250), (-y + 250))); //Use negative Y to simulate a standard Cartesian behaivor
+	        	//M Implementing behavior to scale with the axes, more complexity can be added with more time.
+	            g.draw(new Line2D.Double(((25 * x) + 250), ((25 * -y) + 250), ((25 * x) + 250), ((25 * -y) + 250))); //Use negative Y to simulate a standard Cartesian behaivor
 	        }
 	Graphics2D g;
 }
