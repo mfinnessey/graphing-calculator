e654aa0f02c4dd2009b41a59074e857b860df433
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-10-23 14:01:42 -0700

    Removing Artificats of Old Fix
    Removed artifacts of the old fix, moving over to the new one now.

--- src/GUI.java
+++ src/GUI.java
@@ -410,10 +410,7 @@ public class GUI {
 						System.out.println("Drawing POI ( " + gui.getPois()[k][0] + " , " + gui.getPois()[k][1] + " )");
 					}
 				}
-				//gui.findKeyPoints(gui.getXValues(), gui.getYValues());
-				//graph.drawGraph(gui.getXValues(), gui.getYValues(), gui.unifyKeyPoints(gui.getMins(), gui.getMaxes(), gui.getPois(), gui.getHoles()));
 				gui.setPointsReady(false);
-				// System.out.println(gui.getXValues()[20000]);
 			}
 			else if(gui.getClearDesired() == true) {
 				graph.clear();
