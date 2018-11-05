f450dbc89e90373e06b5025f02f6bb3f362b6bee
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-11-05 13:54:28 -0800

    Graphing Mostly Working
    Literally everything is working except for how the graph wants to cut stuff off at -7 instead of -10 for no apparent reaosn.

--- src/Graph.java
+++ src/Graph.java
@@ -31,19 +31,19 @@ public class Graph {
 		frame.pack();
 		int width = upperX - lowerX;
 		int height = upperY - lowerY;
-		if(width > 1000 || height > 1000) {
+		if(width > 900 || height > 900) {
 			System.out.println("Error: range too great");
 			System.exit(0);
 		}
 		else {
-			graphRatio = 1000/width;
-			if(1000/height < graphRatio) {
-				graphRatio = 1000/height;
+			graphRatio = 900/width;
+			if(900/height < graphRatio) {
+				graphRatio = 900/height;
 			}
 		}
 		
-		frame.setSize(1000, 1000);
-		graphWindow.setSize(1000, 1000);
+		frame.setSize(900, 900);
+		graphWindow.setSize(900, 900);
 		frame.setLocation(600, 0);
 		frame.setVisible(true);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -58,7 +58,7 @@ public class Graph {
 	public void clear() {
 		//M A method to clear the graph
 		g = (Graphics2D) graphWindow.getGraphics();
-		g.clearRect(0, 0, 1000, 1000);
+		g.clearRect(0, 0, 900, 900);
 		colorTracker = 0;
 	}
 	
@@ -81,29 +81,25 @@ public class Graph {
 		 g = (Graphics2D) graphWindow.getGraphics();
 		 System.out.println("DRAW BEGUN");
 		//K draws some coordinate lines
-	    for (int i = 0; i < 1000; i+=graphRatio) {
+	    for (int i = 0; i < 900; i+=graphRatio) {
 	    	g.setColor(Color.lightGray);
-	        g.drawLine(0, i, 1000, i); // X coords
-	        g.drawLine(i, 1000, i, 0); // Y coords
+	        g.drawLine(0, i, 900, i); // X coords
+	        g.drawLine(i, 900, i, 0); // Y coords
 	        //M Issue is that graphRatio isn't being calculated.
-	        System.out.println("I: " + i);
 	    }
-	    System.out.println("COORDINATES DRAWN");
 	        //Draw some axes
 	    g.setColor(Color.BLACK);
-	    g.drawLine(500, 1000, 500, 0); // The Y axis
-	    g.drawLine(0, 500, 1000, 500);  //The X axis
+	    g.drawLine(450, 900, 450, 0); // The Y axis
+	    g.drawLine(0, 450, 900, 450);  //The X axis
 	    //K Writes coordinate numberline
-	    System.out.println("AXES DRAWN");
 	    g.setColor(Color.GRAY);
 	    //M Un-hard coded through here.
 	    for(int i = lowerXLimit; i <= upperXLimit; i++) {
-	    	g.drawString(Integer.toString(i), graphRatio*(i+10), 500); // X coords
+	    	g.drawString(Integer.toString(i), graphRatio*(i+10), 450); // X coords
 	    }
 	    for(int j = lowerYLimit; j <= upperYLimit; j++) {
-	    	g.drawString(Integer.toString(j), 500, 1000-graphRatio*(j+10)); // Y coords
+	    	g.drawString(Integer.toString(j), 450, 900-graphRatio*(j+10)); // Y coords
 	    }
-	    System.out.println("NUMBERS FINISHED");
 	        //K I added some aesthetics but now the graph is kinda messed up because the
 	        //K axes keep on going over the lines when I do more than one line in a graph
 	        //K Other than that it works great
@@ -112,7 +108,7 @@ public class Graph {
 	 public void draw(double x, double y, String color) {
 		 g = (Graphics2D) graphWindow.getGraphics();
 		 g.setColor(Color.decode(color));
-		 g.fillOval((int) ((25 * x) + 247), (int) ((25 * -y) + 247), 6, 6);
+		 g.fillOval((int) ((graphRatio * x) + 447), (int) ((graphRatio * -y) + 447), 6, 6);
 	 }
 	 
 	
@@ -120,7 +116,7 @@ public class Graph {
 	        private void drawPoint(double x, double y) {
 	        	//TODO A method to draw the various lines that make up the graph.
 	        	//M Implementing behavior to scale with the axes, more complexity can be added with more time.
-	            g.draw(new Line2D.Double(((25 * x) + 250), ((25 * -y) + 250), ((25 * x) + 250), ((25 * -y) + 250))); //Use negative Y to simulate a standard Cartesian behaivor
+	            g.draw(new Line2D.Double(((graphRatio * x) + 450), ((graphRatio * -y) + 450), ((graphRatio * x) + 450), ((graphRatio * -y) + 450))); //Use negative Y to simulate a standard Cartesian behaivor
 	        }
 	Graphics2D g;
 }
