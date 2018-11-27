bc0cc8936e690a7940bd418c8c3b74e9d76b5e5c
mrfinnessey <MikeFinnessey@Mike-Finnesseys-MacBook-Pro.local>
2018-11-27 13:45:48 -0800

    Graph Comments
    Commented up Graph some.

--- src/Graph.java
+++ src/Graph.java
@@ -2,7 +2,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 //M Currently issues with graphing complex polynomial functions, likely an issue with syntax for MathEvaluator.
@@ -26,9 +25,12 @@ public class Graph {
 	}
 
 	public void clear() {
-		//M A method to clear the graph
+		//TODO A method to clear the graph
+		//M Getting the graphics object.
 		g = (Graphics2D) graphWindow.getGraphics();
+		//M Drawing a blank rectangle over the area in the window where the graph is drawn.
 		g.clearRect(0, 0, 500, 500);
+		//M Resetting colorTracker, as the graph is now blank again.
 		colorTracker = 0;
 	}
 	
@@ -40,38 +42,52 @@ public class Graph {
 	        if(colorTracker > 7) {
         		colorTracker = 0;
         	}
-        	//M Changing the colors of lines.
+        	//M Changing the colors of lines to be drawn, which iterates with each line drawn.
+	        //M This provides increased visual clarity to the program.
+	        //M Each color is set from an array of hex codes, which is incremented through by colorTracker.
         	g.setColor(Color.decode(lineColors[colorTracker++]));
+        		//M Iterating through each point in the array to draw.
 		        for (int i=1; i < xValues.length; i++) {
-		            drawPoint(xValues[i], yValues[i]);  //Your data gets read here
+		        	//M Passing each individual point to be drawn.
+		            drawPoint(xValues[i], yValues[i]);
 		        }
 	 }
 	 public void draw() {
+		 //TODO A method to draw a graph.
 		 g = (Graphics2D) graphWindow.getGraphics();
 		//K draws some coordinate lines
 	    for (int i = 0; i < 500; i+=25) {
+	    	//M Setting the color of the grid-lines.
 	    	g.setColor(Color.lightGray);
-	        g.drawLine(0, i, 500, i); // X coords
-	        g.drawLine(i, 500, i, 0); // Y coords
+	    	//K X coords
+	        g.drawLine(0, i, 500, i);
+	        //K Y coords
+	        g.drawLine(i, 500, i, 0);
 	    }
-	        //Draw some axes
+	    //K Draw some axes
+	    //M Setting the color of the axes.
 	    g.setColor(Color.BLACK);
-	    g.drawLine(250, 500, 250, 0); // The Y axis
-	    g.drawLine(0, 250, 500, 250);  //The X axis
-	    //K Writes coordinate numberline
+	    //K The Y axis
+	    g.drawLine(250, 500, 250, 0);
+	    //K The X axis
+	    g.drawLine(0, 250, 500, 250);
+	    //K Writes coordinate numberlines
+	    //M Setting the color of the numbers.
 	    g.setColor(Color.GRAY);
+	    //M Iterating through each number that is going to be drawn.
+	    //M The draw methods spread the numbers through the last statement, which "flips" when the numbers become negative.
 	    for(int i = -10; i <=10; i++) {
-	    	g.drawString(Integer.toString(i), 25*(i+10), 250); // X coords
-	    	g.drawString(Integer.toString(i), 250, 500-25*(i+10)); // Y coords
+	    	//M Writing the X coordinates.
+	    	g.drawString(Integer.toString(i), 25*(i+10), 250);
+	    	//M Writing the Y coordinates.
+	    	g.drawString(Integer.toString(i), 250, 500-25*(i+10));
 	    }
-	        
-	        //K I added some aesthetics but now the graph is kinda messed up because the
-	        //K axes keep on going over the lines when I do more than one line in a graph
-	        //K Other than that it works great
 	 }
 	 
 	 public void draw(double x, double y, String color) {
+		 //TODO A method to draw larger dots at key points.
 		 g = (Graphics2D) graphWindow.getGraphics();
+		 //M Setting the color of the key point to be drawn, based on a given hex string.
 		 g.setColor(Color.decode(color));
 		 g.fillOval((int) ((25 * x) + 247), (int) ((25 * -y) + 247), 6, 6);
 	 }
@@ -79,9 +95,9 @@ public class Graph {
 	
 
 	        private void drawPoint(double x, double y) {
-	        	//TODO A method to draw the various lines that make up the graph.
-	        	//M Implementing behavior to scale with the axes, more complexity can be added with more time.
-	            g.draw(new Line2D.Double(((25 * x) + 250), ((25 * -y) + 250), ((25 * x) + 250), ((25 * -y) + 250))); //Use negative Y to simulate a standard Cartesian behaivor
+	        	//TODO A method to draw the various points that make up the graph.
+	        	//Use negative Y to simulate a standard Cartesian behaivor
+	            g.draw(new Line2D.Double(((25 * x) + 250), ((25 * -y) + 250), ((25 * x) + 250), ((25 * -y) + 250))); 
 	        }
 	Graphics2D g;
 }
